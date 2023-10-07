c1c7ae9cce7fafbbf0a4ddac6bfc14b96c2ae171
Isma1108 <ismaelelbasli@gmail.com>
2023-10-07 11:51:10 +0200

    successors

--- src/BicingSuccessorFunction.java
+++ src/BicingSuccessorFunction.java
@@ -1,8 +1,23 @@
 //imports 
+import aima.search.framework.Successor;
+import aima.search.framework.SuccessorFunction;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BicingSuccessorFunction implements SuccessorFunction {
+  public List getSuccessors(Object aState) {
+    ArrayList retVal = new ArrayList();
+    BicingBoard board = new (BicingBoard) aState;
+    BicingHeuristicFunction = new BicingHeuristicFunction();
+
+    //Añadir sucesores a retVal
+    
+
+    return retVal;
 
 
+  }
 
 
 }
