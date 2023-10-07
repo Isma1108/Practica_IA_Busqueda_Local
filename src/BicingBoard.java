4f8b7e488815ec3364efa2e3d17661ba281a09bd
AlexGarcesIsAlreadyTaken <alexgarcesgracia@gmail.com>
2023-10-07 23:09:15 +0200

    Alex's changes on state representation

--- src/BicingBoard.java
+++ src/BicingBoard.java
@@ -1,48 +1,42 @@
 //todos los imports que necesitemos
 import IA.Bicing.Estaciones;
-import java.util.*;
 
+import java.util.*;
 
 
 public class BicingBoard {
 
-  /*Estado
-  * static Estaciones
-  * Vector(F) id-Estacion Origen
-  * Vector(F) id-EstacionDestino,BicisATrasladar [0..2]  (Suma de bicis <= 30)
-  */
-
   //atributos privados:
   private static Estaciones estaciones = null;
-  private int[] origenFurgoneta = {}; //  id-EstacionOrigen
-  private Pair[][] destinosFurgoneta = {}; // F[i] = destino-1{, destino-2}
+  private int[] origenFurgonetas = {};
+  private Pair[][] destinoFurgonetas = {};
 
-    //constructores
-
+  //constructores
+  
   public BicingBoard(Estaciones est, int nfurg, int estado_ini) {
     //Recorro las estaciones y me guardo informacion
     
 
     //Luego, en funcion de que estado_ini queremos
     if (estado_ini == 0) {
-
       //Construimos una solucion inicial
     }
     else {
       //u otra
     }
-    if (estaciones == null) estaciones = est; //
-    origenFurgoneta = new int[nfurg];
-    destinosFurgoneta = new Pair[nfurg][0];
+  
   }
+  
+
+
 };
 
 class Pair {
   public int first;
   public int second;
 
-  public Pair(int first, int second) {
-    this.first = first;
+  public Pair(int f, int s) {
+    this.first = f;
     this.second = second;
   }
 
