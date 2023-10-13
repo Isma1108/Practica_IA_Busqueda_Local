8fc9eb538d688b5d31981fb9851c3dbb7eed4422
Isma1108 <ismaelelbasli@gmail.com>
2023-10-13 23:35:53 +0200

    SA

--- src/BicingSuccessorFunctionSA.java
+++ src/BicingSuccessorFunctionSA.java
@@ -4,12 +4,24 @@ import aima.search.framework.SuccessorFunction;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class BicingSuccessorFunctionSA implements SuccessorFunction {
   public List getSuccessors(Object aState) {
     ArrayList retVal = new ArrayList();
+    BicingBoard board = (BicingBoard) aState;
+    Random myRandom= new Random();
+    int i, j;
 
-    
+    //Nos ahorramos generar todos los successores escojiendo origen y destino al azar
+    //De momento solo uso operador cambiar destino1;
+
+    i = myRandom.nextInt(board.getNumFurgos());
+    j = myRandom.nextInt(board.getNumEstaciones());
+
+    BicingBoard newBoard = new BicingBoard(board.getFurgonetas());
+    newBoard.cambiar_destino1(i,j);
+    retVal.add(new Successor("", newBoard));
 
     return retVal;
   }
